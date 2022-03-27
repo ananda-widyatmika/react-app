d249534bb46d30137f492e278eb5fc7b2b214d84
anandawww <wawidphone@gmail.com>
2022-03-28 02:42:29 +0800

    added song list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './Data.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <img src={data.album.images[0].url} alt="song-img" />
+        <div className='info'>
+          <h1>{data.album.name}</h1>
+          <p>{data.album.artists[0].name}</p>
+        </div>
+        <button>Select</button>
+      </div>
     </div>
   );
 }
